b558f7f181ca9a9ed9b2ee2da2c1405bcfe85811
29yerin-tech <29yerin@gmail.com>
2026-01-26 23:00:27 +0900

    제어 연결완료

--- build/main/src/PlanningControl/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/autonav/aim_ws/src/main/src/PlanningControl/global".split(';') if "/home/autonav/aim_ws/src/main/src/PlanningControl/global" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "lattice_test"
-PROJECT_SPACE_DIR = "/home/autonav/aim_ws/devel"
-PROJECT_VERSION = "0.0.1"
